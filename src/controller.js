8eb41d87f566fe30cbd9a3468069b3352949b6f1
citra puspita rahman <citrapuspita.r@gmail.com>
2021-08-09 05:11:03 +0700

    -add currency, countries, product APis. -add catch

--- src/controller.js
+++ src/controller.js
@@ -1,43 +1,45 @@
 const axios = require('axios');
-const qs = require('qs');
+require('dotenv').config();
 
-const token = process.env.API_TOKEN;
+const token = process.env.NODE_API_TOKEN;
 const config = {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: {
+        contentType: "application/json;charset=utf-8", 
+        Authorization: `Bearer ${token}`,
+    }
 };
 
 createBorrower = async(req,res) =>{
-    
     try{
         const url = `https://dev.orca.accialcapital.com/api/integration/borrowers`;
         const headers = config;
         const data = req.body;
-        await axios.post(url, qs.stringify(data),headers).then(result=>{
-            console.log("result",result);
-            return res.json(result);
+        await axios.post(url,data,headers).then(result=>{
+            console.log(result);
+            return res.json(result?.data);
         }).catch(err=>{
-            console.log(err);
-        }); 
-
+            console.log(err?.response);
+            return res.status(400).send(err?.response);
+        });
     }catch(err){
-        return err;
+        return res.status(400).send(err);
     }
 }
 
 createApplication = async(req,res)=>{
     try{
-        const borrowerId = req.params;
+        const borrowerId = req.params.borrowerId;
         const url = `https://dev.orca.accialcapital.com/api/integration/borrowers/${borrowerId}/applications`;
         const headers = config;
         const data = req.body;
-        await axios.post(url, qs.stringify(data),headers).then(result=>{
-            console.log("result",result);
-            return res.json(result);
+        await axios.post(url, data,headers).then(result=>{
+            console.log(result);
+            return res.json(result?.data);
         }).catch(err=>{
-            console.log(err);
+           console.log(err);
         }); 
     }catch(err){
-        return err;
+        console.log(err);
     }
 };
 
@@ -46,39 +48,92 @@ createLoan = async(req,res)=>{
         const url = `https://dev.orca.accialcapital.com/api/integration/loans`;
         const headers = config;
         const data = req.body;
-        await axios.post(url, qs.stringify(data),headers).then(result=>{
-            console.log("result",result);
-            return res.json(result);
+        await axios.post(url,data,headers).then(result=>{
+            return res.json(result?.data);
         }).catch(err=>{
-            console.log(err);
+           console.log(err?.response);
+           return res.status(400).send(err?.response);
         }); 
 
     }catch(err){
-        return err;
+        return res.status(400).send(err);
     }
 };
 
 createPayment = async(req,res)=>{
     try{
-        const loanID = req.params;
+        const loanID = req.params.loanId;
         const url = `https://dev.orca.accialcapital.com/api/integration/loans/${loanID}/payments`;
         const headers = config;
         const data = req.body;
-        await axios.post(url, qs.stringify(data),headers).then(result=>{
-            console.log("result",result);
-            return res.json(result);
+        await axios.post(url,data,headers).then(result=>{
+            return res.json(result?.data);
         }).catch(err=>{
             console.log(err);
+            return res.status(400).send(err);
+        });
+    }catch(err){
+        console.log(err?.response);
+        return res.status(400).send(err);
+    }
+};
+
+getCountries = async(req,res)=>{
+    try{
+        const url ='https://dev.orca.accialcapital.com/api/integration/tables/countries';
+        const headers = config;
+        
+        await axios.get(url,headers).then(result=>{
+            return res.json(result?.data);
+        }).catch(err=>{
+           return res.status(400).send(err);
         }); 
 
     }catch(err){
-        return err;
+        return res.status(400).send(err?.response);
     }
 };
+
+getProducts = async(req,res)=>{
+    try{
+        const url ='https://dev.orca.accialcapital.com/api/integration/tables/products';
+        const headers = config;
+        
+        await axios.get(url,headers).then(result=>{
+            return res.json(result?.data);
+        }).catch(err=>{
+           return res.status(400).send(err?.response);
+        }); 
+
+    }catch(err){
+        return res.status(400).send(err);
+    }
+};
+
+getCurrencies = async(req,res)=>{
+    try{
+        const url ='https://dev.orca.accialcapital.com/api/integration/tables/currencies';
+        const headers = config;
+        
+        await axios.get(url,headers).then(result=>{
+            return res.json(result?.data);
+        }).catch(err=>{
+           console.log(err?.response);
+           return res.status(400).send(err?.response);
+        }); 
+
+    }catch(err){
+        return res.status(400).send(err);
+    }
+};
+
 
 module.exports={
     createBorrower,
     createApplication,
     createLoan,
-    createPayment
+    createPayment,
+    getCountries,
+    getProducts,
+    getCurrencies
 }
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,8 @@ router.post('/api/borrowers',controller.createBorrower);
 router.post('/api/:borrowerId/applications',controller.createApplication);
 router.post('/api/loans',controller.createLoan);
 router.post('/api/:loanId/payments',controller.createPayment);
+router.get('/api/countries', controller.getCountries);
+router.get('/api/products', controller.getProducts);
+router.get('/api/currencies', controller.getCurrencies);
+
 module.exports = router;
